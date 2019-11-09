7d5eb2303126e926d5178cd01b7be3d8ca3ab7d6
Thanakrit Thongkhamdee <tgrock1997@gmail.com>
2019-11-09 16:10:34 +0700

    index

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Sentry = require('@sentry/node');
-Sentry.init({ dsn: 'https://72c8c5ff156e4006895a1c87a0087f70@sentry.io/1814035' });
+Sentry.init({ dsn: 'https://72c8c5ff156e4006895a1c87a0087f70@sentry.io/1814035'});
 
+Sentry.configureScope(function(scope) {
+    scope.setTag("my-tag", "6030213040");
+    scope.setUser({
+      email: "john.doe@example.com",
+      username: 'Thanakrit Thongkhamdee'
+    });
+   });
 const calculator = require('./calculate');
 
+
+
 myUndefinedFunction();
+
+
